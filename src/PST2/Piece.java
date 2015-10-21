8e3f01aed0fdbb818f9ab1505ed33c9f6be15b87
Porrow <porrow92@gmail.com>
2015-10-21 18:09:24 +0200

    Importation de processing et renommage de Character en Piece

---
+++ src/PST2/Piece.java
@@ -0,0 +1,17 @@
+package PST2;
+
+import processing.core.*; 
+
+public class Piece 
+{
+    public String name;
+    public int type;
+    public int x;
+    public int y;
+    public boolean alive;
+    public PImage icon;
+    
+    public Piece()
+    {
+    }
+}
